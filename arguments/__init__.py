1272aa81b92fdede3ac105dd3814f3cc3fec1ea8
Xinyu Liu <616950640@qq.com>
2026-03-05 14:27:03 +0800

    Adjust pruning levels

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -94,8 +94,8 @@ class OptimizationParams(ParamGroup):
         self.densify_from_iter = 500
         self.densify_until_iter = 12_000
         self.ctprune_ratio = 0.01
-        self.tp_prune_level = 0.5
-        self.recover_level = 0.35
+        self.tp_prune_level = 0.7
+        self.recover_level = 0.4
         self.densify_grad_threshold = 0.0002
         self.depth_l1_weight_init = 1.0
         self.depth_l1_weight_final = 0.01
